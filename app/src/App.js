995e957751815ed6721908cbb84ece82e15a19d1
brunomarques97 <116980642+brunomarques97@users.noreply.github.com>
2023-04-25 22:28:43 -0300

    Criando a primeira tela

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>
+      <main>
+        <section className="texto">
+          <h1>Olá, sou o Bruno Marques.</h1>
+          <p>Desenvolvedor</p>
+        </section>
+        <p className="botao"><span>Veja meu portifolio</span></p>
+        <p className="foto"></p>
+      </main>      
+    </div>
+  );
+}
+
+export default App;
